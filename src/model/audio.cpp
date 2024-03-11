32da2ab225a1d9f207ca19f455aa734d8fbb868c
daniel <desemezi@uwo.ca>
2024-03-11 14:40:47 -0400

    added audio

---
+++ src/model/audio.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Daniel Esemezie on 2024-03-11.
+//
+
+#include "audio.h"
+
+
+
+#include <QMediaPlayer>
+
+namespace Audio {
+
+    void playSound(QString type) {
+        QMediaPlayer *media = new QMediaPlayer();
+        if (type == "die") {
+            media->setMedia(QUrl("qrc:/audio/die.mp3"));
+        } else {
+            Q_ASSERT(type == "success");
+            media->setMedia(QUrl("qrc:/audio/success.mp3"));
+        }
+        media->play();
+    }
+}
---
+++ src/model/audio.h
@@ -0,0 +1,16 @@
+//
+// Created by Daniel Esemezie on 2024-03-11.
+//
+
+#ifndef BITBUDDY_AUDIO_H
+#define BITBUDDY_AUDIO_H
+
+
+#include <QApplication>
+
+namespace Audio {
+    void playSound(QString type);
+}
+
+
+#endif //BITBUDDY_AUDIO_H
