c335493a8dfb7c5119986349de17283663ae8b49
Ryan-h1 <ryan.hecht123@gmail.com>
2024-03-23 15:51:13 -0400

    Add BitBuddy dying

--- game/include/service/FileStorageService.h
+++ game/include/service/FileStorageService.h
@@ -24,8 +24,19 @@ class FileStorageService {
   FileStorageService(FileStorageService &&) = delete;
   FileStorageService &operator=(FileStorageService &&) = delete;
 
+  /***
+   * Saves the BitBuddy to a file
+   *
+   * @param bitBuddy The BitBuddy to save
+   */
   static void saveBitBuddy(const BitBuddy &bitBuddy);
 
+  /***
+   * Loads the BitBuddy from a file and returns a pointer to the newly created BitBuddy
+   *
+   * @param fallBackName The name to use for the creation of a new BitBuddy if the BitBuddy cannot be loaded
+   * @return A pointer to the loaded BitBuddy
+   */
   static BitBuddy *loadBitBuddy(std::string fallBackName);
 
  private:
